bdd8463a1c87b46425685d0d742ee6adf30cb1ca
Viet Nguyen <vietnguyen@Viets-MacBook-Pro.local>
2020-09-27 20:12:29 -0400

    Added static folder

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const routes = require('./routes/routes.js');
-app.use(cors());
+
+const data = require('./routes/api/data');
+app.use('/api/data', data);
 
 const port = process.env.PORT || 5000;
 
 
-app.use(routes);
+//Middleware
+app.use(cors());
 
 
-app.listen(port, () => console.log('Server Started on port' + port));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+
+app.listen(port, () => console.log('Server Started on port ' + port));
--- server/routes/api/data.js
+++ server/routes/api/data.js
@@ -6,24 +6,22 @@ const router = express.Router();
 
 //GET HOMEPAGE
 router.get('/', async function(req, res, next) {
-	const data = await loadDatabase();
-	res.send(await data.find({}).toArray());
+    res.send("this is the root page");
+});
+
+//GET DATA FROM YEAR
+router.get('/:year', async function(req, res, next) {
+    const data = await loadDatabase();
+   
+	res.json(await data.collection(req.params.year).find({}).toArray());
 });
 
 
 async function loadDatabase() {
 	const client = await MongoClient.connect(uri, {useNewUrlParser : true, useUnifiedTopology: true});
 
-return client.db("nyt_database").collection("2000");
+return client.db("nyt_database");
 
 }
-
-
-
-
-
-
-
-
 
 module.exports = router;
